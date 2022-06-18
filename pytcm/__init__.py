c811bf3182774c3068b26128727d66e52ee7408d
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2022-06-17 23:23:47 -0400

    feat(command): Added Command class that holds a command's context

--- pytcm/__init__.py
+++ pytcm/__init__.py
@@ -1,4 +1,5 @@
-from .cmd import execute  # noqa: F401
+from .command import Command  # noqa: F401
 from .options import Explicit, Flag, Implicit, Positional  # noqa: F401
+from .utils import execute  # noqa: F401
 
 __version__ = "0.2.1"
