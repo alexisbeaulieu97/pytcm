9e69d56750cf6bc2bae79446a583ab046debb33e
semantic-release <semantic-release>
2022-06-29 20:41:40 +0000

    0.5.4
    Automatically generated by python-semantic-release

--- pytcm/__init__.py
+++ pytcm/__init__.py
@@ -2,4 +2,4 @@ from .command import Command  # noqa: F401
 from .options import Explicit, Flag, Implicit, Option, Positional  # noqa: F401
 from .utils import execute  # noqa: F401
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
